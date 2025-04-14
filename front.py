94176555d50cb5287ef17776c3229de868c72e63
Juliana April <117351651+HULYANA@users.noreply.github.com>
2025-04-14 15:42:23 +0800

    Initial ui

---
+++ front.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+import tkintermapview
+
+def search_loc():
+    location = search_in.get()
+    if location:
+        map.set_address(location, marker=True)
+
+cordi_map = tk.Tk()
+cordi_map.geometry("1500x800")
+cordi_map.title("Cordillera Region Map Tracker")
+
+main = tk.Frame(cordi_map)
+main.pack(fill="both", expand=True)
+
+map = tkintermapview.TkinterMapView(main, width=1500, height=760)
+map.set_position(16.9083, 122.3941)  
+map.set_zoom(8)
+map.pack(fill="both", expand=True)
+
+search_tf = tk.Frame(main, bg="white", width=500, height=50)
+search_tf.place(relx=0.65, rely=0.05, anchor="w")  
+
+search_in = tk.Entry(search_tf, font=("Times New Roman", 12), width=50, borderwidth=0, highlightthickness=0, relief="flat")
+search_in.pack(side="left", padx=15, pady=5)
+search_in.focus_set()
+
+search_btn = tk.Button(
+    search_tf, text="Search",
+    font=("Times New Roman", 12, "bold italic"),
+    command=search_loc, bg="white", activebackground="LightBlue3",
+    bd=0,  relief="flat", cursor="hand2"
+)
+search_btn.pack(side="left", padx=(0, 15), pady=5)
+
+map.canvas.unbind("<MouseWheel>")
+
+# Check coordinates
+last_pos = map.get_position()
+def check_pos():
+    global last_pos
+    current = map.get_position()
+    if current != last_pos:
+        print(f"Map moved to: {current[0]:.4f}, {current[1]:.4f}")
+        last_pos = current
+    cordi_map.after(300, check_pos)
+
+check_pos()
+
+cordi_map.mainloop()
